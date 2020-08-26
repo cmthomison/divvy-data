0278562c4395fb3cb1eaa103f91f524bf0e0a5d4
Claire Thomison <cmthomison@gmail.com>
2020-08-25 21:14:50 -0500

    Updated API call- currently getting a timeout error

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - Potential feature engineering: station types clustering (commuter vs. recreational)
 - Explore rebalancing
 - Feature engineering including proximity to transit and CTA ride data
+- https://dev.socrata.com/foundry/data.cityofchicago.org/fg6s-gzvg
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-    """
-    Divvy Data Analysis
-    @cmthomison
-    8/25/2020
-    """
+"""
+Divvy Data Analysis
+@cmthomison
+8/25/2020
+"""
 
 
-    import pandas as pd
-    import numpy as np
+import pandas as pd
+import numpy as np
 
-    df = pd.read_json("https://data.cityofchicago.org/resource/fg6s-gzvg.json")
+from sodapy import Socrata
+
+# None indicates no credentials required for public datasets.
+client = Socrata("data.cityofchicago.org", None)
+
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("fg6s-gzvg", limit=2000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
