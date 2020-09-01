f33d56a81c5989d85d7238b857e8d26a58a10f1f
Claire Thomison <cmthomison@gmail.com>
2020-09-01 18:56:05 -0500

    get_mi function to calculate geodesic distance in miles

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ import pandas as pd
 import numpy as np
 import geopandas as gpd
 from shapely.geometry import Point, LineString
+from geopy.distance import geodesic
 
 from sodapy import Socrata
 
 # None indicates no credentials required for public datasets.
 client = Socrata("data.cityofchicago.org", None)
-client.timeout = 60
+client.timeout = 120
 
 # Get bikeshare records with sodapy.
 results = client.get("fg6s-gzvg", limit=1000)
@@ -30,13 +31,26 @@ def get_dist(row):
     to_loc = Point(float(row['to_latitude']),float(row['to_longitude']))
 
     # Create line string.
-    st_path = LineString((from_loc.x,from_loc.y), (to_loc.x,to_loc.y))
+    st_path = LineString([(from_loc.x,from_loc.y), (to_loc.x,to_loc.y)])
 
     return st_path.length
 
+# Function using geopy to get distance (geodesic)
+def get_mi(row):
+
+    # Get tuples
+    from_loc = (float(row['from_latitude']), float(row['from_longitude']))
+    to_loc = (float(row['to_latitude']), float(row['to_longitude']))
+
+    # Get distance
+    dist = geodesic(from_loc, to_loc).miles
+
+    return dist
+
+
 # Testing
 testing = results_df.head()
-testing['dist'] = testing.apply(get_dist, axis=1)
+testing['dist_mi'] = testing.apply(get_mi, axis=1)
 
 # Projection for Chicago
 #.to_crs(epsg=3435)
