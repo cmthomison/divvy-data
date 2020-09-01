897e779012f9a0ca4b927f4704aa9c4645630a4a
Claire Thomison <cmthomison@gmail.com>
2020-08-31 21:56:17 -0500

    Starting rough distance calculation

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Divvy Data Analysis
 import pandas as pd
 import numpy as np
 import geopandas as gpd
+from shapely.geometry import Point, LineString
 
 from sodapy import Socrata
 
@@ -21,5 +22,22 @@ results = client.get("fg6s-gzvg", limit=1000)
 # Convert to pandas DataFrame
 results_df = pd.DataFrame.from_records(results)
 
+# Function to calculate distance between from_location and to_location
+def get_dist(row):
+
+    # Get point tuples.
+    from_loc = Point(float(row['from_latitude']),float(row['from_longitude']))
+    to_loc = Point(float(row['to_latitude']),float(row['to_longitude']))
+
+    # Create line string.
+    st_path = LineString((from_loc.x,from_loc.y), (to_loc.x,to_loc.y))
+
+    return st_path.length
+
+# Testing
+testing = results_df.head()
+testing['dist'] = testing.apply(get_dist, axis=1)
+
 # Projection for Chicago
 #.to_crs(epsg=3435)
+#4326 is WGS84
