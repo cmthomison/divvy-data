de40c12939d88f60027815dd2ab67e9f44bc1ce1
Claire Thomison <cmthomison@gmail.com>
2020-09-01 22:47:26 -0500

    CTA station data pulled and lat/long field generated

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from geopy.distance import geodesic
 from sodapy import Socrata
 from prep import wrangle as wr
 
+# Load data.
 # None indicates no credentials required for public datasets.
 client = Socrata("data.cityofchicago.org", None)
 client.timeout = 120
@@ -24,6 +25,7 @@ results = client.get("fg6s-gzvg", limit=1000)
 # Convert to pandas DataFrame
 results_df = pd.DataFrame.from_records(results)
 
+# Data wrangling/preliminary feature engineering.
 # Calculate distance (in miles) between start and end stations.
 results_df['from_loc'] = results_df.apply(lambda x: wr.lat_long(x['from_latitude'],
                                                                 x['from_longitude']),
@@ -36,6 +38,19 @@ results_df['to_loc'] = results_df.apply(lambda x: wr.lat_long(x['to_latitude'],
 results_df['dist_mi'] = results_df.apply(lambda x: wr.get_mi(x['from_loc'],
                                                              x['to_loc']),
                                                              axis=1)
+
+# Load CTA 'L' station data.
+cta = client.get("8pix-ypme")
+cta_df = pd.DataFrame.from_records(cta)
+
+# Get lat/long tuple for CTA locations.
+cta_df['sloc'] = cta_df.apply(lambda x: wr.lat_long(x['location']['latitude'],
+                                                    x['location']['longitude']),
+                                                    axis=1)
+
+# Next step: determine how to dedupe cta_df on map_id (count California blue
+# line as one stop). For the most part, map_id will work, but there are some
+# special cases.
 
 # Projection for Chicago
 #.to_crs(epsg=3435)
