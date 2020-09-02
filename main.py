02d459127740a4091eec1998fd9dd0f5dfb7313f
Claire Thomison <cmthomison@gmail.com>
2020-09-01 22:27:34 -0500

    Code reorg- functions moved to wrangle.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from shapely.geometry import Point, LineString
 from geopy.distance import geodesic
 
 from sodapy import Socrata
+from prep import wrangle as wr
 
 # None indicates no credentials required for public datasets.
 client = Socrata("data.cityofchicago.org", None)
@@ -23,36 +24,18 @@ results = client.get("fg6s-gzvg", limit=1000)
 # Convert to pandas DataFrame
 results_df = pd.DataFrame.from_records(results)
 
-# Function to create  lat/long tuples
-def lat_long(lat, lon):
+# Calculate distance (in miles) between start and end stations.
+results_df['from_loc'] = results_df.apply(lambda x: wr.lat_long(x['from_latitude'],
+                                                                x['from_longitude']),
+                                                                axis=1)
 
-    lat = float(lat)
-    lon = float(lon)
+results_df['to_loc'] = results_df.apply(lambda x: wr.lat_long(x['to_latitude'],
+                                                              x['to_longitude']),
+                                                              xis=1)
 
-    return (lat, lon)
-
-# Function using geopy to get distance (geodesic)
-def get_mi(from_loc:tuple, to_loc:tuple) -> float:
-
-    # Get distance
-    dist = geodesic(from_loc, to_loc).miles
-
-    return dist
-
-# Testing
-testing = results_df.head()
-
-testing['from_loc'] = testing.apply(lambda x: lat_long(x['from_latitude'],
-                                                       x['from_longitude']),
-                                                       axis=1)
-
-testing['to_loc'] = testing.apply(lambda x: lat_long(x['to_latitude'],
-                                                     x['to_longitude']),
-                                                     axis=1)
-
-testing['dist_mi'] = testing.apply(lambda x: get_mi(x['from_loc'],
-                                                    x['to_loc']),
-                                                    axis=1)
+results_df['dist_mi'] = results_df.apply(lambda x: wr.get_mi(x['from_loc'],
+                                                             x['to_loc']),
+                                                             axis=1)
 
 # Projection for Chicago
 #.to_crs(epsg=3435)
