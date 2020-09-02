e524f163368b2b290e37b1293be611d93b9803b3
Claire Thomison <cmthomison@gmail.com>
2020-09-01 22:27:09 -0500

    Code reorg- functions moved to wrangle script

---
+++ prep/wrangle.py
@@ -0,0 +1,29 @@
+"""
+Divvy Data Wrangling
+@cmthomison
+9/1/2020
+"""
+
+
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+from shapely.geometry import Point, LineString
+from geopy.distance import geodesic
+
+
+# Function to create  lat/long tuples
+def lat_long(lat, lon):
+
+    lat = float(lat)
+    lon = float(lon)
+
+    return (lat, lon)
+
+# Function using geopy to get distance (geodesic)
+def get_mi(from_loc:tuple, to_loc:tuple) -> float:
+
+    # Get distance
+    dist = geodesic(from_loc, to_loc).miles
+
+    return dist
