cfa264f50faf2499703f363fd922882ce4615d4d
Claire Thomison <cmthomison@gmail.com>
2020-09-01 21:37:07 -0500

    lat_long function to generate lat/long tuple

--- main.py
+++ main.py
@@ -35,9 +35,35 @@ def get_mi(row):
 
     return dist
 
+# Function to create  lat/long tuples
+def lat_long(lat, lon):
+
+    lat = float(lat)
+    lon = float(lon)
+
+    return (lat, lon)
+
+# Function using geopy to get distance (geodesic)
+def get_mi(from_loc:tuple, to_loc:tuple) -> float:
+
+    # Get distance
+    dist = geodesic(from_loc, to_loc).miles
+
+    return dist
+
 # Testing
 testing = results_df.head()
 testing['dist_mi'] = testing.apply(get_mi, axis=1)
+
+testing['from_loc'] = testing.apply(lambda x: lat_long(x['from_latitude'],
+                                                       x['from_longitude']),
+                                                       axis=1)
+
+testing['to_loc'] = testing.apply(lambda x: lat_long(x['to_latitude'],
+                                                     x['to_longitude']),
+                                                     axis=1)
+
+testing['from_loc'] = (testing['from_latitude'].astype(float), testing['from_longitude'].astype(float))
 
 # Projection for Chicago
 #.to_crs(epsg=3435)
